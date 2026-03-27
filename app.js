593db4704fcae7af008e58d85c58968433a5f77a
Mathias <4mathias.kodehode@gmail.com>
2026-03-27 21:16:28 +0100

    add rushed final changes

--- app.js
+++ app.js
@@ -1,50 +1,94 @@
-// VARIABLES
-const hero = createElement("div", {
+// TODO: find a better way to organize html elements
+// split stuff into different scripts. and import/export
+// use class/object per (sub)section
+// more seperation of concerns
+// less global variable soup
+// more ways to fake strongly typed types
+// OOP
+
+// VARIABLES (WHAT A MESS!)
+const hero = CreateElement("section", {
     class: "hero",
 });
-const textContainer = createElement("div", {
+const textContainer = CreateElement("div", {
     class: "textContainer",
     parent: hero,
 });
-const title = createElement("h1", {
+const title = CreateElement("h1", {
     parent: textContainer,
-    innerText: "CALL TO ACTION",
+    innerText: "MOST GAMBLERS QUIT JUST BEFORE THEY HIT IT BIG",
 });
-const text = createElement("p", {
+const text = CreateElement("p", {
     parent: textContainer,
-    innerText: "You will never think about this the same again...",
+    innerHTML:
+        'Transform the way you gamble with one service where you can <span class="linethrough">waste</span> ' +
+        "<strong>invest</strong> your money and become giga rich!",
 });
-const buttonTexts = ["BUY NOW", "DOT NOT MISS", "LAST CHANCE", "GONE SOON"];
+const buttonContainer = CreateElement("div", {
+    class: "buttonContainer",
+    parent: textContainer,
+});
+const buttonTexts = ["GAMBLE NOW", "DON'T MISS OUT", "GET RICH", "WIN BIG"];
 let nextButtonTextsIndex = 0;
-const button = createElement("button", {
-    parent: textContainer,
+const buyButton = CreateElement("button", {
+    parent: buttonContainer,
     innerText: buttonTexts[nextButtonTextsIndex++],
+    id: "buyButton",
+    onclick: () => {
+        alert("CONGRATULATIONS YOU JUST GOT SCAMMED!");
+    },
 });
-const productImageContainer = createElement("div", {
+const signUpButton = CreateElement("button", {
+    parent: buttonContainer,
+    id: "signUpButton",
+});
+const signUpButtonText = CreateElement("div", {
+    parent: signUpButton,
+    innerText: "SIGN UP WITH GOOGLE",
+});
+const signUpButtonIcon = CreateElement("img", {
+    parent: signUpButton,
+    src: "./images/googleLogo.png",
+    alt: "google logo",
+    id: "signUpButtonIcon",
+});
+const productImage = CreateElement("div", {
     parent: hero,
-    class: "productImageContainer",
+    class: "productImage",
 });
-const productImage = createElement("img", {
-    parent: productImageContainer,
-    src: "./images/temp.png",
-    alt: "placeholder",
+const productImageTitle = CreateElement("h2", {
+    parent: productImage,
+    innerText: "GIGA GAMBLERS",
 });
+const productImageText = CreateElement("p", {
+    parent: productImage,
+    innerText: "HIGH STAKES ONLY",
+});
+const colours = ["red", "yellow", "pink", "green", "purple", "orange", "blue"];
+let nextColourIndex = 0;
 
 // MAIN
-addButtonEffects();
+AddBuyButtonEffect();
+AddProductImageTextColourEffect();
 
 // FUNCTIONS
-function addButtonEffects() {
+function AddBuyButtonEffect() {
     setInterval(() => {
-        button.classList.toggle("buttonColour");
+        buyButton.classList.toggle("buttonColourOverride");
     }, 200);
     setInterval(() => {
-        button.innerText =
-            buttonTexts[nextButtonTextsIndex++ % buttonTexts.length];
+        buyButton.innerText = buttonTexts[nextButtonTextsIndex++ % buttonTexts.length];
     }, 600);
 }
 
-function createElement(tag, options = {}) {
+function AddProductImageTextColourEffect() {
+    setInterval(() => {
+        productImageText.style.color = colours[nextColourIndex % colours.length];
+        productImageTitle.style.color = colours[++nextColourIndex % colours.length];
+    }, 400);
+}
+
+function CreateElement(tag, options = {}) {
     const element = document.createElement(tag);
     if (options.parent instanceof HTMLElement) {
         options.parent.appendChild(element);
@@ -68,6 +112,9 @@ function createElement(tag, options = {}) {
     }
     if (typeof options.alt === "string") {
         element.alt = options.alt;
+    }
+    if (typeof options.onclick === "function") {
+        element.onclick = options.onclick;
     }
     return element;
 }
